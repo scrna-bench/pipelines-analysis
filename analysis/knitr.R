d610c84a84f449f1474e1efd277812b332c9dc15
Atreya Choudhury <atreyachoudhury@hotmail.com>
2026-03-17 18:00:35 +0100

    update knitr settings

--- analysis/knitr.R
+++ analysis/knitr.R
@@ -1,5 +1,6 @@
 knitr::opts_chunk$set(
   echo = TRUE, message = FALSE, warning = FALSE,
-  dpi = 300, dev = c("png", "pdf"),
+  dpi = 300, fig.retina = 1,
+  dev = c("png", "pdf"), dev.args = list(png = list(type = "cairo")),
   fig.path = Sys.getenv("RMD_FIG_PATH", "figs/")
 )
