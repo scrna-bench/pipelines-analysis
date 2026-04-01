bfa90b8cc69fdba68bd74b197c55c9f2283e64ce
Atreya Choudhury <atreyachoudhury@hotmail.com>
2026-04-01 19:19:31 -0400

    add timings summary in supp 6

--- run.sh
+++ run.sh
@@ -6,17 +6,18 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${ROOT_DIR}"
 
 DOCS=(
-  "analysis/fig1.Rmd"
-  "analysis/fig2.Rmd"
-  "analysis/supp1.Rmd"
-  "analysis/supp2.Rmd"
-  "analysis/supp3.Rmd"
-  "analysis/supp4.Rmd"
-  "analysis/supp5.Rmd"
+  "fig1.Rmd"
+  "fig2.Rmd"
+  "supp1.Rmd"
+  "supp2.Rmd"
+  "supp3.Rmd"
+  "supp4.Rmd"
+  "supp5.Rmd"
+  "supp6.Rmd"
 )
 
 for doc in "${DOCS[@]}"; do
   stem="${doc##*/}"; stem="${stem%.Rmd}"
   RMD_FIG_PATH="../out/figs/" \
-    Rscript -e "source('rv/scripts/activate.R'); rmarkdown::render('${doc}', output_dir = 'out')"
+    Rscript -e "source('rv/scripts/activate.R'); rmarkdown::render('analysis/${doc}', output_dir = 'out')"
 done
